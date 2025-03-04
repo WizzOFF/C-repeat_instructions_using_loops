175539941dd4a7136546ffc428c2cfa3a7c2225a
WizzOFF <maher_off@proton.me>
2025-03-04 23:42:02 +0100

    repeat_instructions_using_loops.c: create a while loop + scanf

--- repeat_instructions_using_loops.c
+++ repeat_instructions_using_loops.c
@@ -1,7 +1,18 @@
+/*
 int compteur = 0;
 
 while (compteur < 10)
 {
     printf("Bienvenue sur OpenClassrooms !\n");
     compteur++;
+}
+*/
+
+
+int nombreEntre = 0;
+
+while (nombreEntre != 47)
+{
+    printf("Tapez le nombre 47 ! ");
+    scanf("%d", &nombreEntre);
 }
